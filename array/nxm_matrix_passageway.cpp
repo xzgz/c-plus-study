a8173621446c45258ffc98070b09774f20317599
xzgz <heyanguang@yahoo.com>
2019-08-29 22:01:07 +0800

    20190829

--- array/nxm_matrix_passageway.cpp
+++ array/nxm_matrix_passageway.cpp
@@ -7,66 +7,89 @@ bool IsCoordValid(const int px, const int py, const int bx, const int by) {
   else return false;
 }
 
-bool IsTherePassageway(const vector<vector<char> >& mat, const int n, const int m, const pair<int, int>& start,
-                       const pair<int, int>& end) {
-  int end_sur_pt_cnt = 0;
-//  if (*(mat + end.first * m + end.second) == '.') {
-//    if (IsCoordValid(end.second, end.first-1, m, n) && *(mat + (end.first-1) * m + end.second) == '.') end_sur_pt_cnt +=1;
-//    if (IsCoordValid(end.second, end.first+1, m, n) && *(mat + (end.first+1) * m + end.second) == '.') end_sur_pt_cnt +=1;
-//    if (IsCoordValid(end.second-1, end.first, m, n) && *(mat + end.first * m + end.second-1) == '.') end_sur_pt_cnt +=1;
-//    if (IsCoordValid(end.second+1, end.first, m, n) && *(mat + end.first * m + end.second+1) == '.') end_sur_pt_cnt +=1;
-//  } else {
-//    end_sur_pt_cnt = 2;
-//  }
-  if (mat[end.first][end.second] == '.') {
-    if (IsCoordValid(end.second, end.first-1, m, n) && mat[end.first-1][end.second] == '.') end_sur_pt_cnt +=1;
-    if (IsCoordValid(end.second, end.first+1, m, n) && mat[end.first+1][end.second] == '.') end_sur_pt_cnt +=1;
-    if (IsCoordValid(end.second-1, end.first, m, n) && mat[end.first][end.second-1] == '.') end_sur_pt_cnt +=1;
-    if (IsCoordValid(end.second+1, end.first, m, n) && mat[end.first][end.second+1] == '.') end_sur_pt_cnt +=1;
-  } else {
-    end_sur_pt_cnt = 2;
-  }
-  if (end_sur_pt_cnt < 2) return false;
+bool IsRoad(const int px, const int py, const vector<vector<char> >& mat,
+			const vector<vector<bool> >& is_used) {
+  int by = mat.size();
+  int bx = by ? mat[0].size() : 0;
+  if (0 <= px && px < bx && 0 <= py && py < by
+	  && mat[py][px] == '.' && !is_used[py][px]) return true;
+  else return false;
+}
 
-  bool is_used[n][m] = {0};
+bool IsTherePassagewayBFS(const vector<vector<char> >& mat, const int n, const int m,
+						  const pair<int, int>& start, const pair<int, int>& end) {
+  int end_sur_pt_cnt = 0;
+  int dx[4] = { 0, 0, -1, 1 };
+  int dy[4] = { -1, 1, 0, 0 };
+  if (!(IsCoordValid(end.second, end.first, m, n) &&
+		IsCoordValid(start.second, start.first, m, n))) return false;
+  if (mat[end.first][end.second] == '.') {
+	for (int i = 0; i < 4; ++i) {
+	  if (IsCoordValid(end.second+dx[i], end.first+dy[i], m, n)
+		  && mat[end.first+dy[i]][end.second+dx[i]] == '.') end_sur_pt_cnt += 1;
+	}
+  } else end_sur_pt_cnt = 2;
+  if (end_sur_pt_cnt < 2) return false;
+  if (start == end) return true;
+
+  bool is_used[n][m] = { false };
   queue<pair<int, int> > roads;
+  roads.push(start);
   pair<int, int> front;
-  if (IsCoordValid(start.second, start.first, m, n)) roads.push(start);
+  pair<int, int> point;
   while (!roads.empty()) {
     int node_count = roads.size();
     for (int i = 0; i < node_count; ++i) {
       front = roads.front();
       roads.pop();
-
-      pair<int, int> point(front.first-1, front.second);
-      if (point == end) return true;
-      if (IsCoordValid(point.second, point.first, m, n) && !is_used[point.first][point.second]
-          && mat[point.first][point.second] == '.') {
-        roads.push(point);
-        is_used[point.first][point.second] = true;
-      }
-      point = make_pair(front.first+1, front.second);
-      if (point == end) return true;
-      if (IsCoordValid(point.second, point.first, m, n) && !is_used[point.first][point.second]
-          && mat[point.first][point.second] == '.') {
-        roads.push(point);
-        is_used[point.first][point.second] = true;
-      }
-      point = make_pair(front.first, front.second-1);
-      if (point == end) return true;
-      if (IsCoordValid(point.second, point.first, m, n) && !is_used[point.first][point.second]
-          && mat[point.first][point.second] == '.') {
-        roads.push(point);
-        is_used[point.first][point.second] = true;
-      }
-      point = make_pair(front.first, front.second+1);
-      if (point == end) return true;
-      if (IsCoordValid(point.second, point.first, m, n) && !is_used[point.first][point.second]
-          && mat[point.first][point.second] == '.') {
-        roads.push(point);
-        is_used[point.first][point.second] = true;
-      }
+      for (int j = 0; j < 4; ++j) {
+		point = make_pair(front.first + dy[j], front.second + dx[j]);
+		if (IsCoordValid(point.second, point.first, m, n) &&
+			!is_used[point.first][point.second] && mat[point.first][point.second] == '.') {
+		  if (point == end) return true;
+		  roads.push(point);
+		  is_used[point.first][point.second] = true;
+		}
+	  }
     }
+  }
+
+  return false;
+}
+
+bool IsTherePassagewayDFS(vector<vector<char> >& mat, const int n, const int m,
+						  const pair<int, int>& start, const pair<int, int>& end) {
+  int end_sur_pt_cnt = 0;
+  int dx[4] = { 0, 0, -1, 1 };
+  int dy[4] = { -1, 1, 0, 0 };
+  if (!(IsCoordValid(end.second, end.first, m, n) &&
+		IsCoordValid(start.second, start.first, m, n))) return false;
+  if (mat[end.first][end.second] == '.') {
+    for (int i = 0; i < 4; ++i) {
+      if (IsCoordValid(end.second+dx[i], end.first+dy[i], m, n)
+		  && mat[end.first+dy[i]][end.second+dx[i]] == '.') end_sur_pt_cnt +=1;
+	}
+  } else end_sur_pt_cnt = 2;
+  if (end_sur_pt_cnt < 2) return false;
+  if (start == end) return true;
+
+  vector<vector<bool> > is_used(n, vector<bool>(m, false));
+  stack<pair<int, int> > roads;
+  mat[start.first][start.second] = '.';
+  pair<int, int> cur = start;
+  pair<int, int> top;
+  int j = 0;
+  while (IsRoad(cur.second, cur.first, mat, is_used) || !roads.empty()) {
+	while (IsRoad(cur.second, cur.first, mat, is_used)) {
+	  if (cur == end) return true;
+	  roads.push(cur);
+	  is_used[cur.first][cur.second] = true;
+	  cur = make_pair(cur.first+dy[j], cur.second+dx[j]);
+	}
+	if (j == 0) top = roads.top();
+	++j;
+	cur = make_pair(top.first+dy[j], top.second+dx[j]);
+	if (j == 3) { j = 0; roads.pop(); }
   }
 
   return false;
@@ -80,31 +103,25 @@ int main() {
   for (int i = 0; i < t; ++i) {
     cin >> n;
     cin >> m;
-//    void *mat = calloc(n*m, sizeof(char));
     vector<vector<char> > mat(n, vector<char>(m, 0));
     for (int j = 0; j < n; ++j) {
       for (int k = 0; k < m; ++k) {
         char c;
         cin >> c;
-//        *((char *)mat + j * m + k) = c;
         mat[j][k] = c;
       }
     }
-    int coord;
-    cin >> coord;
-    start.first = coord - 1;
-    cin >> coord;
-    start.second = coord - 1;
-    cin >> coord;
-    end.first = coord - 1;
-    cin >> coord;
-    end.second = coord - 1;
+    int y1, x1, y2, x2;
+    cin >> y1 >> x1 >> y2 >> x2;
+    start.first = y1 - 1;
+    start.second = x1 - 1;
+    end.first = y2 - 1;
+    end.second = x2 - 1;
 
-//    bool is_there_passageway = IsTherePassageway((char *)mat, n, m, start, end);
-    bool is_there_passageway = IsTherePassageway(mat, n, m, start, end);
+//	bool is_there_passageway = IsTherePassagewayBFS(mat, n, m, start, end);
+	bool is_there_passageway = IsTherePassagewayDFS(mat, n, m, start, end);
     if (is_there_passageway) result[i] = "YES";
     else result[i] = "NO";
-//    free(mat);
   }
   for (int i = 0; i < t; ++i) {
     cout << result[i] << endl;
--- classic-problem/kmp.cpp
+++ classic-problem/kmp.cpp
@@ -2,21 +2,20 @@
 
 using namespace std;
 
-vector<int> GetNext(const string& t) {
+static vector<int> GetNext(const string& t) {
   vector<int> next(t.size(), 0);
+  int t_len = t.size();
   next[0] = -1;
   int j = 0, k = -1;
 
-  while (j < t.size()) {
+  while (j < t_len) {
     if (k == -1 || t[j] == t[k]) {
       ++j;
       ++k;
+      // t[j]==t[k] means t(0)t(1)...t(k)==t(j-k)t(j-k+1)...t(j)
       if (t[j] == t[k]) next[j] = next[k];
       else next[j] = k;
-    }
-	else {
-      k = next[k];
-    }
+    } else k = next[k];
   }
 
   return next;
@@ -24,70 +23,23 @@ vector<int> GetNext(const string& t) {
 
 int KMP(const string& s, const string& t) {
   int i = 0, j = 0;
+  int s_len = s.size();
+  int t_len = t.size();
   vector<int> next = GetNext(t);
 
-  while (i < s.size() && j < t.size()) {
-    if (j == -1 || s[i] == t[j]) { ++i, ++j; }
+  while (i < s_len && j < t_len) {
+    if (j == -1 || s[i] == t[j]) ++i, ++j;
     else j = next[j];
   }
 
-  if (j == t.size()) return i - j;
+  if (j == t_len) return i - j;
   else return -1;
 }
 
-static void get_next(const char* dest, int next[]) {
-  /* 计算回溯值 */
-
-  int j = 0;
-  int k = -1;
-  next[0] = -1;
-  int dest_len = strlen(dest);
-
-  while(j < dest_len) {
-    if(k == -1 || dest[j] == dest[k]) {
-      ++j;
-      ++k;
-      if(dest[j] != dest[k]) {
-        next[j] = k;
-      } else {
-        next[j] = next[k];
-      }
-      next[j] = k;
-    } else {
-      k = next[k];
-    }
-  }
-}
-
-int kmp(const char *src, const char* dest) {
-  assert(src && dest);
-
-  int i = 0;
-  int j = 0;
-  const int src_len = strlen(src);
-  const int dest_len = strlen(dest);
-  int next[dest_len];
-
-  get_next(dest, next);
-
-  i = 0;
-  while(i < src_len && j < dest_len) {
-    if(j == -1 || src[i] == dest[j]) {
-      i++;
-      j++;
-    } else {
-      j = next[j];
-    }
-  }
-  return j >= dest_len ? i - dest_len : -1;
-}
-
 int main() {
-  string s = "asdffas";
-//  string t = "dffa";
-  string t = "abcaababc";
-//  int loc = KMP(s, t);
-  int loc = kmp(s.data(), t.data());
+  string s = "abcdabcabcda";
+  string t = "abcabcd";
+  int loc = KMP(s, t);
   cout << loc;
 
   return 0;
