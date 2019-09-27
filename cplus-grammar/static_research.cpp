0c26b034a4692c8fc2811ce75564739aae0e2e61
xzgz <heyanguang@yahoo.com>
2019-09-27 12:31:53 +0800

    20190927

--- cplus-grammar/static_research.cpp
+++ cplus-grammar/static_research.cpp
@@ -9,13 +9,28 @@ class A {
 //  static int a = 5;  // error: ISO C++ forbids in-class initialization of non-const static member ‘A::a’
   static int a;
   const static int b = 1;
-  int c;
+  static const int c;
 };
 
 //A::a = 3;  // error: ‘a’ in ‘class A’ does not name a type
+
 int A::a = 3;
 
+// error: ‘static’ may not be used when defining (as opposed to declaring) a static data member
+//const static int A::c = 2;
+
+// error: conflicting declaration ‘int A::c’
+// error: declaration of ‘const int A::c’ outside of class is not definition
+//int A::c = 2;
+
+int const A::c = 2;
+
+void Initialize() {
+//  int A::a = 3;  // error: qualified-id in declaration before ‘=’ token
+}
+
 int main() {
+  Initialize();
   A oa;
 //  A oa(0);  // can be compiled
 //  A oa = A();  // the same as "A oa;"
@@ -24,5 +39,8 @@ int main() {
   cout << oa.a << endl;
   oa.a = 4;
   cout << A::a << endl;
+  A::a = 10;
+  cout << A::a << endl;
   cout << A::b << endl;
+  cout << A::c << endl;
 }
