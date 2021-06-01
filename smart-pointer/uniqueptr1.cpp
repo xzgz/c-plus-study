a0623426d8559fc90a4e522aedfcddceadfd9b25
xzgz <heyanguang@yahoo.com>
2021-06-01 17:02:56 +0800

    20210601

--- smart-pointer/uniqueptr1.cpp
+++ smart-pointer/uniqueptr1.cpp
@@ -14,6 +14,7 @@
 #include <dirent.h>  // for opendir(), ...
 #include <cstring>   // for strerror()
 #include <cerrno>    // for errno
+
 using namespace std;
 
 class DirCloser
@@ -26,8 +27,10 @@ class DirCloser
     }
 };
 
-int main()
-{
+template <typename T>
+using uniquePtr = std::unique_ptr<T, void(*)(T*)>;      // alias template
+
+int main() {
     // open current directory:
     unique_ptr<DIR,DirCloser> pDir(opendir("."));
 
@@ -38,4 +41,59 @@ int main()
         cout << "process " << filename << endl;
         //...
     }
+
+    std::string* sp = new std::string("hello");
+    std::unique_ptr<std::string> up1(sp);
+//    std::unique_ptr<std::string> up2(sp); // ERROR: up1 and up2 own same data
+
+    class ClassA {
+    public:
+        string name;
+        explicit ClassA(const string& nm) : name(nm) {}
+        ~ClassA() {
+            cout << "delete " << name << endl;
+        }
+    };
+    std::unique_ptr<ClassA> upca1(new ClassA("upca1"));
+//    std::unique_ptr<ClassA> upca2(upca1);             // ERROR: not possible
+    std::unique_ptr<ClassA> upca3(std::move(upca1));    // OK
+    cout << "std::unique_ptr<ClassA> upca3(std::move(upca1));" << endl;
+
+    std::unique_ptr<ClassA> upca4;                      // create another unique_ptr
+//    upca4 = upca1;                                    // ERROR: not possible
+    upca4 = std::move(upca1);                           // transfers ownership from upca1 to upca4
+    std::unique_ptr<ClassA> upca5(new ClassA("upca5"));
+    // move assign the unique_ptr
+    // delete object owned by upca5
+    // transfer ownership from upca1 to upca5
+    upca5 = std::move(upca1);
+    cout << "upca5 = std::move(upca1);" << endl;
+
+    std::unique_ptr<ClassA> ptr;
+    ptr = std::unique_ptr<ClassA>(new ClassA("ptr"));   // OK, delete old object
+    ptr = nullptr;                                              // deletes the associated object, if any
+    cout << "ptr = nullptr;" << endl;
+
+//    std::unique_ptr<std::string> up(new std::string[10]);         // runtime ERROR
+    std::unique_ptr<std::string[]> up(new std::string[10]);     // OK
+//    std::cout << *up << std::endl;                                // ERROR: * not defined for arrays
+    std::cout << up[0] << std::endl;                                // OK
+
+    std::unique_ptr<int, void(*)(int*)> upi1(new int[10], [](int *p) {
+        cout << "void(*)(int*) -- delete [] p, p[2] = " << p[2] << endl;
+        delete [] p;
+    });
+    std::unique_ptr<int, std::function<void(int*)> > upi2(new int[10], [](int *p) {
+        cout << "std::function<void(int*)> -- delete [] p, p[2] = " << p[2] << endl;
+        delete[] p;
+    });
+    auto del_fun = [](int *p) {
+        cout << "auto del_fun -- delete [] p, p[2] = " << p[2] << endl;
+        delete [] p;
+    };
+    std::unique_ptr<int, std::function<void(int*)> > upi3(new int[10], del_fun);
+    uniquePtr<int> upi4(new int[10], [](int *p) {
+        cout << "uniquePtr<int> -- delete [] p, p[2] = " << p[2] << endl;
+        delete[] p;
+    });
 }
